8eea54977087dd5dfd8b9531ca60618ac6ef001f
Zorin Egor <zorin-ep@mail.ru>
2023-09-21 19:59:34 +0800

    init

---
+++ Mortal Fibonacci Rabbits/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val N: Int = 85 //прошло месяцев
+    val M: Int = 19 //сколько живут кролики
+    val bunnies = LongArray(N)
+
+    for (month in 0 until N) {
+        if (month < 2) {
+            bunnies[month] = 1
+        } else if (month < M) {
+            bunnies[month] = bunnies[month - 2] + bunnies[month - 1]
+        } else if (month == M || month == M + 1) {
+            bunnies[month] = bunnies[month - 2] + bunnies[month - 1] - 1
+        } else {
+            bunnies[month] = bunnies[month - 2] + bunnies[month - 1] - bunnies[month - M - 1]
+        }
+    }
+
+    println(bunnies[N - 1])
+}
